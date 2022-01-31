1c2da880cda046d25db844b9979e74027763f4da
Leo <leonardo.licon@gmail.com>
2022-01-31 16:15:19 -0700

    style

--- src/components/DestinationsBox.tsx
+++ src/components/DestinationsBox.tsx
@@ -9,12 +9,12 @@ const DestinationsBox = () => {
   const [result, setResult] = useState<any>();
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {  
-    if(loading){
-        fetchTrain().catch((error) => console.log(error));
+  useEffect(() => {
+    if (loading) {
+      fetchTrain().catch((error) => console.log(error));
     }
-    else{
-        updatePriorities()
+    else {
+      updatePriorities()
     }
   }, [loading, result]);
 
@@ -27,24 +27,45 @@ const DestinationsBox = () => {
 
   const updatePriorities = () => {
     for (var i = 0; i < result.length; i++) {
-        result[i].priority = i + 1;
-        updateDestination(result[i], result[i].name, result[i].priority);
+      result[i].priority = i + 1;
+      updateDestination(result[i], result[i].name, result[i].priority);
     }
   }
 
   return (
-    <div>
-        {!loading ?
-            <List
-                values={result.map(a => a.name)}
-                onChange={({ oldIndex, newIndex }) =>
-                    setResult(arrayMove(result, oldIndex, newIndex))
-                }
-                renderList={({ children, props }) => <ul {...props}>{children}</ul>}
-                renderItem={({ value, props }) => <li {...props}>{value}</li>}
-            />
-            : null
-        }
+    <div
+      style={{
+        maxWidth: '300px',
+        margin: '10px ',
+        backgroundColor: '#F7F7F7',
+        padding: '3em', display: 'inline-block'
+      }}
+    > <h3>Destination</h3>
+      {!loading ?
+        <List lockVertically
+          values={result.map(a => a.name)}
+          onChange={({ oldIndex, newIndex }) =>
+            setResult(arrayMove(result, oldIndex, newIndex))
+          }
+          renderList={({ children, props, isDragged }) => <ul {...props} style={{ padding: 0, cursor: isDragged ? 'grabbing' : undefined }}
+          >{children}</ul>}
+          renderItem={({ value, props, isDragged, isSelected }) => <li {...props}
+            style={{
+              ...props.style,
+              padding: '1.5em',
+              margin: '0.5em 0em',
+              listStyleType: 'none',
+              cursor: isDragged ? 'grabbing' : 'grab',
+              border: '2px solid #CCC',
+              boxShadow: '3px 3px #AAA',
+              color: '#333',
+              borderRadius: '5px',
+              fontFamily: 'Arial, "Helvetica Neue", Helvetica, sans-serif',
+              backgroundColor: isDragged || isSelected ? '#EEE' : '#FFF', width: 250
+            }}>{value}</li>}
+        />
+        : null
+      }
     </div>
   );
 }
